d51c0c0163d32e21581d9db46199e5924829aead
saurabh daware <saurabhdaware99@gmail.com>
2018-07-04 16:47:31 +0530

    Read done

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse,Http404,HttpRequest
+from django.http import HttpResponse,Http404,HttpRequest,JsonResponse
 from .models import StaffUser
 import json
+from django.core import serializers
 
 # Create your views here.
 def index(request):
@@ -11,11 +12,13 @@ def create(request):
     user_info = StaffUser(username=request.POST['username'],email=request.POST['email'],phonenum=request.POST['phonenum'],password=request.POST['password'])
     user_info.save()
 
-
     return HttpResponse('User %s created ' % user_info.username)
+    values = []
 
 def read(request):
-    return HttpResponse("Read")
+    user_info = StaffUser.objects.all()
+    json_data = serializers.serialize('json', user_info)
+    return HttpResponse(json_data)
 
 def update(request):
     return HttpResponse("Update")
