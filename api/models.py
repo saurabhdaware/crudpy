0fa3575d5d90b2f70036c917ef66c3fecb7038a7
saurabh daware <saurabhdaware99@gmail.com>
2018-07-05 11:24:52 +0530

    primary key update

--- api/models.py
+++ api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class StaffUser(models.Model):
-    username = models.CharField(max_length=20)
-    email = models.CharField(max_length=20,primary_key=True)
+    username = models.CharField(max_length=20,primary_key=True)
+    email = models.CharField(max_length=20)
     phonenum = models.CharField(max_length=14)
     password = models.CharField(max_length=20)
--- api/views.py
+++ api/views.py
@@ -9,30 +9,33 @@ def index(request):
     return HttpResponse("Index page of apis")
 
 def create(request):
-    user_info = StaffUser(username=request.POST['username'],email=request.POST['email'],phonenum=request.POST['phonenum'],password=request.POST['password'])
-    user_info.save()
-
     response = {}
-    response['username'] = user_info.username
-    response['email'] = user_info.email
-    json_data = json.dumps(response)
-    return HttpResponse(json_data)
+    if(request.method == 'POST'):
+        user_info = StaffUser(username=request.POST['username'],email=request.POST['email'],phonenum=request.POST['phonenum'],password=request.POST['password'])
+        user_info.save()
+        response['username'] = user_info.username
+        response['email'] = user_info.email
+        json_data = json.dumps(response)
+        return HttpResponse(json_data)
+    else:
+        response['error'] = 'Couldnt read the data of user'
+        return HttpResponse(json.dumps(response))
 
 def read(request):
     user_info = StaffUser.objects.all()
     json_data = serializers.serialize('json', user_info)
     return HttpResponse(json_data)
 
-def update(request,user_email):
+def update(request,user_username):
     data = {}
     if(request.method == 'POST'):
-        user_info = StaffUser.objects.get(email=user_email)
+        user_info = StaffUser.objects.get(username=user_username)
         if('password' in request.POST):
             user_info.password = request.POST['password']
         if('phonenum' in request.POST):
             user_info.phonenum = request.POST['phonenum']
-        if('username' in request.POST):
-            user_info.username = request.POST['username']
+        if('email' in request.POST):
+            user_info.username = request.POST['email']
         user_info.save()
         data['success'] = 'Data of %s edited successfully' % user_email
         return HttpResponse(json.dumps(data))
